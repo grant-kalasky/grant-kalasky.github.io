57d8828908fd941228fd0e0412223c79482e6aa8
grantkalasky <grant.kalasky@berkeley.edu>
2016-07-29 12:41:11 -0700

    scroller again

---
+++ js/scroller.js
@@ -0,0 +1,17 @@
+var maxIndex = $('.scroll span').length,
+    curIndex = 0;
+
+$(function(){
+  spin();
+})
+  
+function spin(){
+$('.scroll').animate({ scrollTop: curIndex * $('.scroll span').height() + 'px' }, 300, function(){
+  if(curIndex + 1 === maxIndex){
+    curIndex = 0;
+  } else {
+    curIndex++;
+  }
+  setTimeout(function(){ spin() }, 2000);
+});
+}
